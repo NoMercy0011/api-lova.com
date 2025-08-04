a80519b8a7d1e55187d9853609d97d3c6c407ab5
JapsKim <lucrandriaoelison@gmail.com>
2025-08-04 11:29:39 +0300

    api-Lova commit 2025-08-04

--- config/database.php
+++ config/database.php
@@ -46,9 +46,9 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql-api-lova.alwaysdata.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_CENTRAL', 'lova_central'),                                                  
+            'database' => env('DB_CENTRAL', 'api-lova_central'),                                                  
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
